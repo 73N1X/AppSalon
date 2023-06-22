837403158bd3cb48ad883bb1b4175541ea1b7d03
ZeNix <zenix691@gmail.com>
2023-06-22 12:20:17 -0700

    modified:   composer.json 	modified:   composer.lock 	modified:   includes/app.php 	modified:   includes/database.php

--- includes/app.php
+++ includes/app.php
@@ -1,8 +1,14 @@
 <?php 
 
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable('../');
+$dotenv->load();
+
 require 'funciones.php';
 require 'database.php';
-require __DIR__ . '/../vendor/autoload.php';
+
+$db = conDB();
 
 // Conectarnos a la base de datos
 use Model\ActiveRecord;
--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,18 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'mint', 'appsalon_mvc');
+function conDB(): mysqli
+{
+    $db = new mysqli(
+        $_ENV['DB_HOST'],
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS'],
+        $_ENV['DB_BD']
+    );
 
+    if (!$db) {
+        echo 'Error no se pudo conectar';
+        exit;
+    }
 
-if (!$db) {
-    echo "Error: No se pudo conectar a MySQL.";
-    echo "errno de depuración: " . mysqli_connect_errno();
-    echo "error de depuración: " . mysqli_connect_error();
-    exit;
+    return $db;
 }
